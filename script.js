5f434336da30b8bc07daa3399d5f95cfec0f57c9
MichaelMuya <michael.kimuri@strathmore.edu>
2024-01-21 01:24:47 +0300

    feat: add smooth scroll to section

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function alt() {
-    const menu_link = documet.querySelector(".menu_links")
-    const icon = documet.querySelector(".icon_click")
+    const menu_link = document.querySelector(".menu_links");
+    const icon = document.querySelector(".icon_click");
 
-    menu_link.classList.toggle("open")
-    icon.classList.toggle()
+    menu_link.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function scrollToSection(about){
+    const section = document.getElementById(action);
+    if(section){
+        section.scrollIntoView({behavior: 'smooth'})
+    }
 }
